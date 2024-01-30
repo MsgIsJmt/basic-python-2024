8975e05ef822cf4c89560ea544b3233df738f45e
MsgIsJmt <tnwls43888@naver.com>
2024-01-30 17:18:12 +0900

    2일차 종료

--- day02/test11.tuple.py
+++ day02/test11.tuple.py
@@ -4,8 +4,9 @@
 ## 튜플 - 리스트 변형, 리스트는 조작가능 [] but 튜플은 조작 불가 ()
 tuple_a = (1,3,5,7,9)
 
+# for i in tuple_a:
+    # print(i)
 
+print(tuple_a[1])
 
-
-
-
+tuple_a[1] = 4
---
+++ day02/test13.range.py
@@ -0,0 +1,20 @@
+# file: test13_range.py
+# desc: 리스트 범위지정
+
+list_a = [1,2,3,4,5,6,7,8,9,10]
+print(list_a)
+
+# 범위 지정 range(n)
+print(range(5))
+print(list(range(1, 6))) # 1!5
+print(list(range(2, 21, 2))) # 2부터 20(+1)까지 2씩 증가
+print(list(range(1, 20, 2))) # 1부터 19까지 2씩 증가
+print(list(range(10, 1, -1))) # countdown
+
+list_a = list(range(1, 101))
+print(list_a)
+
+list_a = [i for i in range(1, 101)]
+print(list_a)
+
+
